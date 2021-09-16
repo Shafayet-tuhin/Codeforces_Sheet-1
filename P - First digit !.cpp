ebaf0230c59d859d0d266f89a65443a3b05cb31a
SHAFAYET TUHIN <85475193+Shafayet-tuhin@users.noreply.github.com>
2021-09-17 01:18:35 +0600

    Create P - First digit !.cpp

---
+++ P - First digit !.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int a,b;
+cin>>a;
+b = a/1000;
+if (b % 2 ==0)
+{
+    cout << "EVEN"<<endl;
+}
+else{cout << "ODD"<<endl;}
+return 0;
+}
