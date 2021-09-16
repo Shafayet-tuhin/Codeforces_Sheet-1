bf32c1b586e7a15ed4603e939a860bc4b213a6d4
SHAFAYET TUHIN <85475193+Shafayet-tuhin@users.noreply.github.com>
2021-09-17 01:22:38 +0600

    Create V - Comparison.cpp

---
+++ V - Comparison.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b;
+char s;
+cin >>a>>s>>b;
+    if (s == 62 && a > b)
+    {
+        cout << "Right";
+    }
+  else if (s == 60 && a < b)
+    {
+        cout << "Right";
+    }
+     else if (s == 61 && a == b)
+    {
+        cout << "Right";
+    }
+ else
+ {
+       cout << "Wrong";
+ }
+    return 0;
+
+}
