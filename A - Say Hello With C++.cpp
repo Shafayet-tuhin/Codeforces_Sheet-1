3997d80856d94cf59490612202783d6536d6d957
SHAFAYET TUHIN <85475193+Shafayet-tuhin@users.noreply.github.com>
2021-09-17 00:05:06 +0600

    Create A - Say Hello With C++.cpp

---
+++ A - Say Hello With C++.cpp
@@ -0,0 +1,25 @@
+/*
+𝓐𝓾𝔃𝓾𝓫𝓲𝓵𝓵𝓪𝓱 𝓜𝓲𝓷𝓪𝓼𝓱𝓪𝓲𝓽𝓪𝓷 𝓝𝓲𝓻𝓪𝓳𝓮𝓮𝓶
+𝓑𝓲𝓼𝓶𝓲𝓵𝓵𝓪𝓱𝓲𝓻 𝓡𝓪𝓱𝓶𝓪𝓷𝓲𝓻 𝓡𝓪𝓱𝓲𝓶
+*/
+#include<bits/stdc++.h>
+#define optimize ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define time cerr<<"Time : "<<(float)clock()/CLOCKS_PER_SEC<<" s"<<endl
+#define ll long long
+#define ul unsigned long long 
+#define dd double
+#define ff float
+#define ss string
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
+freopen("D:/inputoutput/input.txt", "r", stdin);
+freopen("D:/inputoutput/output.txt", "w", stdout);
+#endif
+
+    string s;
+    cin >> s ;
+    cout << "Hello, " <<s << endl;
+    return 0 ;
+}
