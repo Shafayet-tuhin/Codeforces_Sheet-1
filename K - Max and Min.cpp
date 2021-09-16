a34f1210a6b0dfb83897d8e07674d1305510a19f
SHAFAYET TUHIN <85475193+Shafayet-tuhin@users.noreply.github.com>
2021-09-17 01:14:26 +0600

    Create K - Max and Min.cpp

---
+++ K - Max and Min.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int a,b,c,temp;
+
+  cin >>a>>b>>c;
+
+   if (a > b)
+    {
+      temp = a;
+      a = b;
+      b = temp;
+    }
+  if (a > c)
+    {
+      temp = a;
+      a = c;
+      c = temp;
+    }
+  if (b > c)
+    {
+      temp = b;
+      b = c;
+      c = temp;
+    }
+cout << a << " " <<c<< endl;
+
+    return 0;
+}
